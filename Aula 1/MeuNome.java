af0a5eeab62f7d663886027d4cdd3263e46bc831
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-08-21 19:22:41 -0300

    Update MeuNome.java

--- Aula 1/MeuNome.java
+++ Aula 1/MeuNome.java
@@ -1,5 +1,5 @@
-public class MeuNome {
+public class MyFirstApp {
 	public static void main(String [] arg){
-		System.out.println("Meu nome é " + arg[0]);
+		System.out.println("Meu nome é " + arg[0] +" "+ arg[1]);
 	}
 }
