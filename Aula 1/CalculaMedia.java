f734ee75cdd9f7d1beedbd87a0e591ec3132960b
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-08-21 19:35:04 -0300

    Update CalculaMedia.java

--- Aula 1/CalculaMedia.java
+++ Aula 1/CalculaMedia.java
@@ -2,6 +2,6 @@ public class CalculaMedia {
 	public static void main(String [] arg){
 	int A = Integer.parseInt(arg[0]);
 	int B = Integer.parseInt(arg[1]);
-	System.out.println((A + B)/2);
+	System.out.println((A + B)/2.0);
 	}
 }
