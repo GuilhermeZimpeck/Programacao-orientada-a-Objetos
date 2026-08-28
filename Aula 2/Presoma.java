5d8c3650d02a6200990c602abb4bd1b47979fd90
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-08-28 19:41:46 -0300

    Create Presoma.java

---
+++ Aula 2/Presoma.java
@@ -0,0 +1,12 @@
+public class Presoma {
+
+	public static void main(String[] args) {
+		int i=5;
+		int x= i++;
+		System.out.println(x +" "+ i);
+		i=5;
+		x= ++i;
+		System.out.println(x +" "+ i);
+	}
+
+}
