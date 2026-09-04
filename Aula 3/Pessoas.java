aed9ce4ae43f50a8ec67b79fdef38d475fa7993e
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-09-04 19:24:36 -0300

    Create Pessoas.java

---
+++ Aula 3/Pessoas.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pessoas {
+
+	public static void main(String[] arg) {
+		Pessoa p = new Pessoa();
+		p.setName("guilherme");
+		p.setSalario(2000);
+		System.out.println(p.getSalario());
+		System.out.println(p.getName());
+		
+	}
+}
