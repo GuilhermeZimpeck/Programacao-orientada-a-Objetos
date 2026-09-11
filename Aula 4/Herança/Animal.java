b1ab38b3d51d9c0aaa3bdf588940897c6a941c22
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-09-11 19:12:16 -0300

    Create Animal.java

---
+++ Aula 4/Herança/Animal.java
@@ -0,0 +1,38 @@
+
+public class Animal {
+	private String nome;
+	private int idade;
+	private double peso;
+	private String cor;
+	private String corOlhos;
+	
+	public String getCorOlhos() {
+		return corOlhos;
+	}
+	public void setCorOlhos(String corOlhos) {
+		this.corOlhos = corOlhos;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
