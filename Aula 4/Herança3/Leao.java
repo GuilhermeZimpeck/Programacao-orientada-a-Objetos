55dc8fdb74faf49ca322427a3e340792e88f016d
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-09-11 21:03:41 -0300

    Create Leao.java

---
+++ Aula 4/Herança3/Leao.java
@@ -0,0 +1,8 @@
+public class Leao extends Animal{
+	public void rugir() {
+		System.out.println("LEAO: " + getNome() + " ROOAAAAAAARRRRRR");
+	}
+	public void correr() {
+		System.out.println("Leao correndo");
+	}
+}
