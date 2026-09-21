a7f96ff327aa95d0b73340408da21c49f87a28cf
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-09-20 23:57:01 -0300

    Update Main.java

--- Use a Cabeça Java/Capitulo 3/Cachorro/Main.java
+++ Use a Cabeça Java/Capitulo 3/Cachorro/Main.java
@@ -1,4 +1,15 @@
 class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog();
+        dog1.name = "Bart";
+
+        Dog[] myDogs = new Dog[3];
+
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        myDogs[0].name = "Fred";
+        myDogs[1] = "Marge";
+
     }
